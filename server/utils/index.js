c7a67c29c5c51fa493a4509ab7a82bad093924fc
ryuusennka <369563963@qq.com>
2021-11-01 03:11:39 +0800

    没必要用 babel 转

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,18 +1,24 @@
 /*
  * @Author: ryuusennka
  * @Date: 2021-02-28 23:44:54
- * @LastEditors: ryuusennka
- * @LastEditTime: 2021-03-19 15:34:30
- * @FilePath: /quick-test/server/utils/index.js
+ * @LastEditors   : ryuusennka
+ * @LastEditTime  : 2021-11-01 03:10:31
+ * @FilePath      : /socketio-without-db/server/utils/index.js
  * @Description:
  */
 const path = require('path');
-export const handlePromiser = promise =>
+const handlePromiser = promise =>
   promise.then(res => [null, res]).catch(err => [err, null]);
 
-export const resolve = dir => {
+const resolve = dir => {
   return path.join(__dirname, '..', dir); // 针对server文件夹
 };
 
 // [响应用户]返回错误信息
-export const errors = msg => ({ errors: msg });
+const errors = msg => ({ errors: msg });
+
+module.exports = {
+  handlePromiser,
+  resolve,
+  errors,
+};
